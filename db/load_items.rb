a4729249a5cc37b765337a61036efc05b8a63974
Tony Gambone <tonygambone@gmail.com>
2009-09-09 16:10:25 -0400

    Lists of fonts and cheeses, and a script to load them.

---
+++ db/load_items.rb
@@ -0,0 +1,15 @@
+#
+# load cheeses and fonts files into the database
+# run with script/runner
+#
+# does not delete current database items, just adds new ones
+#
+{'cheeses.txt'=>true, 'fonts.txt'=>false}.each do |file, is_cheese|
+  File.open(Rails.root.join('db', file)).each_line do |line|
+    begin
+      Item.new(:name=>line.strip, :cheese=>is_cheese).save      
+    rescue
+      # ignore unique index errors
+    end
+  end
+end
