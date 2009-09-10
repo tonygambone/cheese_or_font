31a4ae688962e67d51cfb694c5ff0b4d03182ebf
Tony Gambone <tonygambone@gmail.com>
2009-09-10 08:35:03 -0400

    Rails 2.2.2 compatibility changes to run on Dreamhost.

--- db/load_items.rb
+++ db/load_items.rb
@@ -5,7 +5,7 @@
 # does not delete current database items, just adds new ones
 #
 {'cheeses.txt'=>true, 'fonts.txt'=>false}.each do |file, is_cheese|
-  File.open(Rails.root.join('db', file)).each_line do |line|
+  File.open(File.join(Rails.root, 'db', file)).each_line do |line|
     begin
       Item.new(:name=>line.strip, :cheese=>is_cheese).save      
     rescue
