4b1d475e4f2a493b825909057fac36ae4311c071
Tony Gambone <tonygambone@gmail.com>
2009-09-11 20:09:37 -0400

    Print the name of each item that is added.

--- db/load_items.rb
+++ db/load_items.rb
@@ -7,7 +7,8 @@
 {'cheeses.txt'=>true, 'fonts.txt'=>false}.each do |file, is_cheese|
   File.open(File.join(Rails.root, 'db', file)).each_line do |line|
     begin
-      Item.new(:name=>line.strip, :cheese=>is_cheese).save      
+      Item.new(:name=>line.strip, :cheese=>is_cheese).save  
+      puts line.strip      
     rescue
       # ignore unique index errors
     end
