9c22b9e1e5b689c744bce9da0cd482763067340a
Tony Gambone <tonygambone@gmail.com>
2009-09-13 10:10:42 -0400

    Use the ActiveRecord session store.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090909133120) do
+ActiveRecord::Schema.define(:version => 20090913140711) do
 
   create_table "items", :force => true do |t|
     t.string   "name",                          :null => false
@@ -21,5 +21,15 @@ ActiveRecord::Schema.define(:version => 20090909133120) do
   end
 
   add_index "items", ["name"], :name => "index_items_on_name", :unique => true
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
 end
